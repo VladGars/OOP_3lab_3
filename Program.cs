2e3c806d02c072849b35ff699551ac892ab05ba0
VladGars <kn1b23.harasevych@kpnu.edu.ua>
2025-06-15 11:34:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class KeyValueStore<TKey, TValue>
+{
+    private Dictionary<TKey, TValue> _store = new Dictionary<TKey, TValue>();
+
+    public void Add(TKey key, TValue value)
+    {
+        _store[key] = value;
+    }
+
+    public TValue GetValueByKey(TKey key)
+    {
+        return _store[key];
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var userNames = new KeyValueStore<int, string>();
+        userNames.Add(1, "Анна");
+        userNames.Add(2, "Богдан");
+        Console.WriteLine($"Ім'я користувача з ID 1: {userNames.GetValueByKey(1)}");
+
+
+        var productPrices = new KeyValueStore<string, double>();
+        productPrices.Add("Молоко", 35.50);
+        productPrices.Add("Хліб", 22.00);
+        Console.WriteLine($"Ціна на молоко: {productPrices.GetValueByKey("Молоко")}");
+    }
+}
